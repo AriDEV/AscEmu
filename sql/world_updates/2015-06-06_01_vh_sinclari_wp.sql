fa8b9408ce32ae1b8f9bcd620f31b067b079a01d
Rachael Hepworth <evairfairy@gmail.com>
2015-06-06 09:37:51 +0100

    Fix broken database version

--- sql/world_updates/2015-06-06_01_vh_sinclari_wp.sql
+++ sql/world_updates/2015-06-06_01_vh_sinclari_wp.sql
@@ -5,4 +5,4 @@ UPDATE `creature_waypoints` SET `waittime`='5000', `forwardemoteoneshot`='1', `f
 INSERT INTO `ascemu_world`.`creature_waypoints` (`spawnid`, `waypointid`, `position_x`, `position_y`, `position_z`, `waittime`, `flags`, `forwardemoteoneshot`, `forwardemoteid`, `backwardemoteoneshot`, `backwardemoteid`, `forwardskinid`, `backwardskinid`) VALUES ('125515', '5', '1820.03', '803.97', '44.36', '0', '0', '0', '0', '0', '0', '0', '0');
 INSERT INTO `ascemu_world`.`creature_waypoints` (`spawnid`, `waypointid`, `position_x`, `position_y`, `position_z`, `waittime`, `flags`, `forwardemoteoneshot`, `forwardemoteid`, `backwardemoteoneshot`, `backwardemoteid`, `forwardskinid`, `backwardskinid`) VALUES ('125515', '6', '1814.70', '804.16', '44.36', '0', '0', '0', '0', '0', '0', '0', '0');
 
-UPDATE `world_db_version` SET `LastUpdate` = '2015-06_06_01_vh_sinclari_wp';
+UPDATE `world_db_version` SET `LastUpdate` = '2015-06-06_01_vh_sinclari_wp';
