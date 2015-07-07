486863b1ddb6bf47624491b3438d1aabd8dbf721
Rachael Hepworth <evairfairy@gmail.com>
2015-07-07 18:19:06 +0100

    Refactor: Move spline trajectory time to MoveSpline class

--- src/world/AIInterface.h
+++ src/world/AIInterface.h
@@ -550,7 +550,7 @@ class SERVER_DECL AIInterface : public Arcemu::IUpdatable
         //uint32 m_currentSplineUpdateCounter;
         //float m_currentSplineFinalOrientation;
         //float m_splinetrajectoryVertical;
-        uint32 m_splinetrajectoryTime;
+        //uint32 m_splinetrajectoryTime;
         //uint32 m_currentSplineTotalMoveTime;
         uint32 m_splinePriority;
 
